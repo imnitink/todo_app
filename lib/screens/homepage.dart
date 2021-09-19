563d9f948e936a832d2ed502747d8f23ea6ee04f
Nithin Kumar <agathsya.nithin@dt.local>
2021-09-20 04:37:38 +0530

    Second commit

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,5 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'package:todo_app/database_helper.dart';
+import 'package:todo_app/models/task.dart';
 import 'package:todo_app/screens/taskpage.dart';
 import 'package:todo_app/widgets.dart';
 
@@ -11,103 +16,393 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   DatabaseHelper _dbHelper = DatabaseHelper();
 
+  String filterType = "today";
+  DateTime today = new DateTime.now();
+  String taskPop = "close";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          color: Color(0xFFF6F6F6),
-          child: Stack(
+      appBar: AppBar(
+        backgroundColor: Color(0xfff96060),
+        elevation: 0,
+        title: Text(
+          "Todo list",
+          style: TextStyle(fontSize: 30),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.short_text,
+              color: Colors.white,
+              size: 30,
+            ),
+          )
+        ],
+      ),
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: 32.0,
-                      bottom: 32.0,
+              Container(
+                height: 70,
+                color: Color(0xfff96060),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        InkWell(
+                          onTap: () {},
+                          child: Text(
+                            "Today",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          height: 4,
+                          width: 120,
+                          color: (filterType == "today")
+                              ? Colors.white
+                              : Colors.transparent,
+                        )
+                      ],
                     ),
-                    child: Image(
-                      image: AssetImage('assets/images/logo.png'),
-                    ),
-                  ),
-                  Expanded(
-                    child: FutureBuilder(
-                      initialData: [],
-                      future: _dbHelper.getTasks(),
-                      builder: (context, snapshot) {
-                        return ScrollConfiguration(
-                          behavior: NoGlowBehaviour(),
-                          child: ListView.builder(
-                            itemCount: snapshot.data.length,
-                            itemBuilder: (context, index) {
-                              return GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => Taskpage(
-                                        task: snapshot.data[index],
-                                      ),
-                                    ),
-                                  ).then(
-                                    (value) {
-                                      setState(() {});
-                                    },
-                                  );
-                                },
-                                child: TaskCardWidget(
-                                  title: snapshot.data[index].title,
-                                  desc: snapshot.data[index].description,
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        InkWell(
+                          onTap: () {},
+                          child: Text(
+                            "Monthly",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          height: 4,
+                          width: 120,
+                          color: (filterType == "monthly")
+                              ? Colors.white
+                              : Colors.transparent,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: FutureBuilder(
+                  initialData: [],
+                  future: _dbHelper.getTasks(),
+                  builder: (context, snapshot) {
+                    return ScrollConfiguration(
+                      behavior: NoGlowBehaviour(),
+                      child: ListView.builder(
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Taskpage(
+                                    task: snapshot.data[index],
+                                  ),
                                 ),
+                              ).then(
+                                (value) {
+                                  setState(() {});
+                                },
                               );
                             },
-                          ),
-                        );
-                      },
-                    ),
-                  )
-                ],
-              ),
-              Positioned(
-                bottom: 24.0,
-                right: 0.0,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => Taskpage(
-                                task: null,
-                              )),
-                    ).then((value) {
-                      setState(() {});
-                    });
+                            child: taskWidget(
+                                generateRandomColor(),
+                                snapshot.data[index].title,
+                                snapshot.data[index].description,
+                                snapshot.data[index]),
+                          );
+                        },
+                      ),
+                    );
                   },
-                  child: Container(
-                    width: 60.0,
-                    height: 60.0,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: [Color(0xFF7349FE), Color(0xFF643FDB)],
-                          begin: Alignment(0.0, -1.0),
-                          end: Alignment(0.0, 1.0)),
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Image(
-                      image: AssetImage(
-                        "assets/images/add_icon.png",
+                ),
+              ),
+              Container(
+                height: 110,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      bottom: 0,
+                      child: Container(
+                        height: 90,
+                        width: MediaQuery.of(context).size.width,
+                        color: Color(0xff292e4e),
+                        padding: EdgeInsets.all(20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Container(
+                              child: Column(
+                                children: [
+                                  Icon(
+                                    Icons.check_circle,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    "My Task",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Container(
+                              child: Column(
+                                children: [
+                                  Icon(
+                                    Icons.menu,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    "Menu",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Container(
+                              width: 80,
+                            ),
+                            Container(
+                              child: Column(
+                                children: [
+                                  Icon(
+                                    Icons.content_paste,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    "Quick",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Container(
+                              child: Column(
+                                children: [
+                                  Icon(
+                                    Icons.account_circle,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    "Profile",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
+                    Positioned(
+                      bottom: 25,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        height: 80,
+                        width: 80,
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft,
+                              colors: [Color(0xfff96060), Colors.red],
+                            ),
+                            shape: BoxShape.circle),
+                        child: Center(
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Taskpage(
+                                          task: null,
+                                        )),
+                              ).then((value) {
+                                setState(() {});
+                              });
+                            },
+                            child: Text(
+                              "+",
+                              style:
+                                  TextStyle(fontSize: 40, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               )
             ],
           ),
+        ],
+      ),
+    );
+  }
+
+
+  Slidable taskWidget(
+      Color color, String title, String description, Task task) {
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.3,
+      child: Container(
+        height: 80,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.03),
+              offset: Offset(0, 9),
+              blurRadius: 20,
+              spreadRadius: 1)
+        ]),
+        child: Row(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              height: 25,
+              width: 25,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  border: Border.all(color: color, width: 4)),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+                Text(
+                  description,
+                  style: TextStyle(color: Colors.grey, fontSize: 18),
+                )
+              ],
+            ),
+            Expanded(
+              child: Container(),
+            ),
+            Container(
+              height: 50,
+              width: 5,
+              color: color,
+            )
+          ],
         ),
       ),
+      secondaryActions: [
+        IconSlideAction(
+          caption: "Edit",
+          color: Colors.white,
+          icon: Icons.edit,
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Taskpage(
+                  task: task,
+                ),
+              ),
+            ).then(
+              (value) {
+                setState(() {});
+              },
+            );
+          },
+        ),
+        IconSlideAction(
+          caption: "Delete",
+          color: color,
+          icon: Icons.edit,
+          onTap: () {
+            _showDialog(task.id);
+          },
+        )
+      ],
+    );
+  }
+
+  Color generateRandomColor() {
+    Random random = Random();
+    // Pick a random number in the range [0.0, 1.0)
+    double randomDouble = random.nextDouble();
+
+    return Color((randomDouble * 0xFFFFFF).toInt()).withOpacity(1.0);
+  }
+
+  void _showDialog(int id) {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Delete Task"),
+          content: new Text("Are you sure you want to delete this task"),
+          actions: <Widget>[
+            Row(
+              children: [
+                new FlatButton(
+                  child: new Text("Confirm"),
+                  onPressed: () async {
+                    if (id != 0) {
+                      await _dbHelper.deleteTask(id);
+                      setState(() {
+                        _dbHelper.getTasks();
+                      });
+                      Navigator.of(context).pop();
+                    }
+                  },
+                ),
+                new FlatButton(
+                  child: new Text("Cancel"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            )
+
+            // usually buttons at the bottom of the dialog
+          ],
+        );
+      },
     );
   }
 }
